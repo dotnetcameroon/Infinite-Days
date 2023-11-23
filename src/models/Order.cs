be3bb4bb9d9126122d732708bfff55e9123d756b
Djoufson CHE BENE <djouflegran@gmail.com>
2023-11-23 09:20:14 +0100

    code: Update the Order entity
    The Order entity is responsible for managing its product list, and its state (either processed or not yet)

--- src/models/Order.cs
+++ src/models/Order.cs
@@ -4,6 +4,24 @@ public class Order
 {
     private readonly List<Product> _products = [];
     public int Id { get; set; }
+    public bool Processed { get; private set; }
     public IReadOnlyList<Product> Products => _products.ToArray();
     public decimal TotalPrice => Products.Sum(p => p.Price);
+
+    public void AddProduct(Product product)
+    {
+        // TODO: Implement the method that adds a product to this order
+        throw new NotImplementedException();
+    }
+
+    public void RemoveProduct(int id)
+    {
+        // TODO: Implement the method that removes a specific product from the order based on its Id
+        throw new NotImplementedException();
+    }
+
+    public void ChangeOrderStatus(bool status)
+    {
+        Processed = status;
+    }
 }
