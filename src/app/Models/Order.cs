3f862ac6b0be81b8a81f10d94ab58908547cb6bd
Djoufson CHE BENE <djouflegran@gmail.com>
2023-11-23 07:49:28 +0100

    code: Create the Product and Order entities

---
+++ src/app/Models/Order.cs
@@ -0,0 +1,9 @@
+namespace app.Models;
+
+public class Order
+{
+    private readonly List<Product> _products = [];
+    public int Id { get; set; }
+    public IReadOnlyList<Product> Products => _products.ToArray();
+    public decimal TotalPrice => Products.Sum(p => p.Price);
+}
---
+++ src/app/Models/Product.cs
@@ -0,0 +1,8 @@
+namespace app.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+}
