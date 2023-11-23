e2826a8d622c17634528b0d2385e20f3bf8b730b
Djoufson CHE BENE <djouflegran@gmail.com>
2023-11-23 09:23:15 +0100

    code: Implement the OrderService that will be used by the client to process orders

---
+++ src/app/Services/OrderService.cs
@@ -0,0 +1,33 @@
+using app.Repositories;
+using models;
+
+namespace app.Services;
+
+public class OrderService
+{
+
+    private readonly IOrderRepository _orderRepository;
+
+    public OrderService(IOrderRepository orderRepository)
+    {
+        _orderRepository = orderRepository;
+        _orderRepository.OrderProcessed += HandleOrderProcessed;
+    }
+
+    public Task ProcessOrderAsync(int orderId)
+    {
+        // Asynchronously process the order
+        // Simulates a long CPU bound calculation
+        // TODO: Retrieve the order with the corresponding Id from the repository
+        Thread.Sleep(1400);
+        // TODO: Change the status of this order
+        throw new NotImplementedException();
+    }
+
+    private void HandleOrderProcessed(Order order)
+    {
+        // Notify the user that an order has been processed
+        // TODO: Write the order informations to the console
+        throw new NotImplementedException();
+    }
+}
