01cd79952a73e8da3251b24fcbf7a010b816b8d7
Djoufson CHE BENE <djouflegran@gmail.com>
2023-11-23 12:24:52 +0100

    feat: Implement the `ViewProducts` feature

--- src/ui/Features/ViewProducts.cs
+++ src/ui/Features/ViewProducts.cs
@@ -7,6 +7,18 @@ internal partial class Feature
 {
     public static bool ViewProducts(IServiceProvider sp)
     {
+        var productsRepository = sp.GetRequiredService<IProductsRepository>();
+        var products = productsRepository.GetAllProducts();
+
+        Console.WriteLine("Here is the list of all our products");
+        foreach (var product in products)
+        {
+            Console.WriteLine(product.Id);
+            Console.WriteLine(product.Name);
+            Console.WriteLine(product.Price);
+            Console.WriteLine("___________________________________________");
+        }
+
         return false;
     }
 }
