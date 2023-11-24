bd469c3a6679947966ca55a61b01eb2345801fd8
Djoufson CHE BENE <djouflegran@gmail.com>
2023-11-24 10:13:15 +0100

    feat: Implement the `ViewOrders` feature ui part

--- src/ui/Features/ViewOrders.cs
+++ src/ui/Features/ViewOrders.cs
@@ -1,9 +1,23 @@
+using app.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace ui.Features;
 
 internal partial class Feature
 {
     public static bool ViewOrders(IServiceProvider sp)
     {
+        var orderRepository = sp.GetRequiredService<IOrderRepository>();
+        var products = orderRepository.GetAllOrders();
+
+        Console.WriteLine("These are all the registered orders:");
+        foreach (var product in products)
+        {
+            Console.WriteLine($"Id:\t{product.Id}");
+            Console.WriteLine($"Total Price:\t{product.TotalPrice}");
+            Console.WriteLine("___________________________________________");
+        }
+
         return false;
     }
 }
