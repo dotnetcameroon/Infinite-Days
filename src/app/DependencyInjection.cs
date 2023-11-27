bab96b6f277426b1bbba88f8773040f7dfc4684c
Djoufson CHE BENE <djouflegran@gmail.com>
2023-11-27 09:31:13 +0100

    fix: Dependency Injection services registration
    Properly register the ProductService to the DI container

--- src/app/DependencyInjection.cs
+++ src/app/DependencyInjection.cs
@@ -8,6 +8,7 @@ public static class DependencyInjection
     public static IServiceCollection RegisterApp(this IServiceCollection services)
     {
         services.AddScoped<OrderService>();
+        services.AddScoped<ProductService>();
         return services;
     }
 }
