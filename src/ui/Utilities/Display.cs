f03de8afec49675ec047fcad67afa48c7f6948d7
Djoufson CHE BENE <djouflegran@gmail.com>
2023-12-07 17:33:27 +0100

    docs: Documenting `Display` class and methods

--- src/ui/Utilities/Display.cs
+++ src/ui/Utilities/Display.cs
@@ -1,19 +1,39 @@
 namespace ui.Utilities;
 
+/// <summary>
+/// A static class responsible to user I/O operations through the Console.
+/// </summary>
 internal static class Display
 {
+    /// <summary>
+    /// Prints each option from the provided collection to the console as a menu.
+    /// </summary>
+    /// <param name="options">The collection of available options.</param>
     public static void PrintMenu(IEnumerable<(int index, string name)> options)
     {
         foreach (var (index, name) in options)
             Console.WriteLine($"[{index}] {name}");
     }
 
+    /// <summary>
+    /// Read an input from the user, responding to the specified prompt and convert the result to <typeparamref name="T"/> type.
+    /// </summary>
+    /// <param name="prompt">The prompt user has to respond to.</param>
+    /// <typeparam name="T">The type of input supported for this prompt. The type 'T' must be a not null primitive type</typeparam>
+    /// <returns>The valid <typeparamref name="T"/> value the user entered.</returns>
     public static T Read<T>(string? prompt = "")
         where T : struct, IConvertible
     {
         return Read<T>(obj => (true, string.Empty), prompt);
     }
 
+    /// <summary>
+    /// Read an input from the user, responding to the specified prompt and convert the result to <typeparamref name="T"/> type
+    /// </summary>
+    /// <param name="validateInput">A delegate specifying specific rules on how to validate the prompt after the conversion</param>
+    /// <param name="prompt">The prompt user has to respond to</param>
+    /// <typeparam name="T">The type of input supported for this prompt. The type 'T' must be a not null primitive type</typeparam>
+    /// <returns>The valid <typeparamref name="T"/> value the user entered</returns>
     public static T Read<T>(Func<T, (bool,string)> validateInput, string? prompt = "")
         where T : struct, IConvertible
     {
@@ -48,6 +68,10 @@ internal static class Display
         return result;
     }
 
+    /// <summary>
+    /// Prints an error like message to the console without a line break at the end
+    /// </summary>
+    /// <param name="message"></param>
     public static void WriteError(string message)
     {
         Console.ForegroundColor = ConsoleColor.Red;
